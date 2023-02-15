4437e405195315d9c4a96d8f031229e41d771292
gracie.sharma <graciesharma0@gmail.com>
2023-02-15 22:43:28 +0545

    initial

---
+++ captcha.php
@@ -0,0 +1,8 @@
+<?php
+session_start();
+$string = "123";
+$_SESSION["vercode"] = $string;
+$font = 'arial.ttf';
+$width = 200;
+$height = 80;
+?>
---
+++ includes/footer.php
@@ -0,0 +1,10 @@
+   <section class="footer-section">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                   &copy;Online Library Management System |<a href="https://github.com/graciesharma" target="_blank"  > Designed by : Kumar Pandule</a> 
+                </div>
+
+            </div>
+        </div>
+    </section>
